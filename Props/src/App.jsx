679dfd9f5e6548f3517de5eafce9a95f13e2db20
Sanup Divakaran <sanukyr@gmail.com>
2024-08-31 12:25:36 +0530

    minor updates

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -4,10 +4,10 @@ import Child1 from "./Component/child1";
 function App() {
   let a = 10;
   return (
-    <>
+    <div style={{backgroundImage: `url("https://via.placeholder.com/500")`}}>
       <h2>Hey</h2>
       <Child1 a={a} />
-    </>
+    </div>
   );
 }
 
